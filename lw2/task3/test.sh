701b4c484e38b8387af4a9c6c3f0fab807bba30c
Andruxa Ilyin <andruxa.ilyin@gmail.com>
2014-11-11 23:57:46 +0300

    Добавлены тесты. Добавлена поддержка матриц чисел с плавающей точкой. Реализована возможность задавать матрицы в файле. При выводе на экран у коэффициентов матрицы выводится лишь 3 знака после запятой.

---
+++ lw2/task3/test.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+exe=./multmatrix
+
+#Запуск программы без параметров
+result=`($exe)`
+errorLevel=$?
+expectation=`cat test/invalid_count_of_arguments.txt`
+if [ "$errorLevel" -ne 1 ] || [ "$result" != "$expectation" ]; then
+    echo "Failed checking without parameters."
+    exit
+fi
+
+#Проверка на обработку ошибки с открытием файла
+result=`($exe test/correct_m1.txt invalid_file.txt)`
+errorLevel=$?
+expectation=`cat test/invalid_file.txt`
+if [ "$errorLevel" -ne 1 ] || [ "$result" != "$expectation" ]; then
+    echo "Failed checking invalid file opening."
+    exit
+fi
+
+#Проверка на feof в файле матрицы
+result=`($exe test/correct_m1.txt test/invalid_m2.txt)`
+errorLevel=$?
+expectation=`cat test/invalid_matrix_3x3.txt`
+if [ "$errorLevel" -ne 1 ] || [ "$result" != "$expectation" ]; then
+    echo "Failed checking for feof."
+    exit
+fi
+
+#Проверка перемножения матриц
+result=`($exe test/correct_m1.txt test/correct_m2.txt)`
+errorLevel=$?
+expectation=`cat test/correct_result1.txt`
+if [ "$errorLevel" -ne 0 ] || [ "$result" != "$expectation" ]; then
+    echo "Failed checking for matrix multiplication."
+    exit
+fi
+
+
+echo "Program testing succeeded!"
