56f54782692820ef155ad4e391c6ff92489e0f1a
Andruxa Ilyin <andruxa.ilyin@gmail.com>
2014-11-19 22:58:04 +0300

    Убрал лишние константы. Добавил makefile Добавил .gitignore

--- lw1/task1/print_digits.cpp
+++ lw1/task1/print_digits.cpp
@@ -2,10 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-    const int MAX_NUMBER = 100;
-    const int STEP = -2; 
-
-    for (int i = MAX_NUMBER; i > 0; i = i + STEP)
+    for (int i = 100; i > 0; i -= 2)
     {
         printf("%d", i);
         if (i != 2)
