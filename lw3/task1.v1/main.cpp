572a78ea9464779dfcff3f5a865a732050c47959
Andruxa Ilyin <andruxa.ilyin@gmail.com>
2014-11-28 19:40:57 +0300

    ×òåíèå ÷èñåë èç cin áûë èçìåíåí. Òåïåðü ñ÷èòûâàíèå îñóùåñòâÿëåòñÿ ïðè êîíñòðóèðîâàíèè ýëåìåíòà vector.
    vector<double> numbers(istream_iterator<double>(cin), (istream_iterator<double>()));

--- lw3/task1.v1/main.cpp
+++ lw3/task1.v1/main.cpp
@@ -7,11 +7,9 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	vector<double> numbers;
 	istream_iterator<double> start(cin);
 	istream_iterator<double> end;
-	auto where = back_inserter(numbers);
-	copy(start, end, where);
+	vector<double> numbers(start, end);
 
     return 0;
 }
