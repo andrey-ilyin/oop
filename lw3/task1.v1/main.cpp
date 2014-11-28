d3469459d63ef8ab8db765111d64937a40ac20b7
Andruxa Ilyin <andruxa.ilyin@gmail.com>
2014-11-28 20:12:41 +0300

    Выводятся только положительные значения.

--- lw3/task1.v1/main.cpp
+++ lw3/task1.v1/main.cpp
@@ -8,7 +8,10 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     vector<double> numbers(istream_iterator<double>(cin), (istream_iterator<double>()));
-    copy(numbers.begin(), numbers.end(), ostream_iterator<double>(cout, " "));
+
+
+    ostream_iterator<double> output(cout, " ");
+    copy_if(numbers.begin(), numbers.end(), output, [] (double item){ return (item > 0); });
 
     return 0;
 }
