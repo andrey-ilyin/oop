66ffc1b77052d836411bbc1793f1188c6c81eae2
Andruxa Ilyin <andruxa.ilyin@gmail.com>
2014-11-28 19:24:40 +0300

    Initial commit. Reading numbers from cin was added.

---
+++ lw3/task1.v1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	vector<double> numbers;
+	double number;
+
+	while (cin >> number )
+	{
+		numbers.push_back(number);
+	}
+    
+
+
+    return 0;
+}
