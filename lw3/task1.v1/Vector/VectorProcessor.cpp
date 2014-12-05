e6ce8484ff2505d3692f12a6481fd0efffd9204c
Andruxa Ilyin <andruxa.ilyin@gmail.com>
2014-12-05 20:34:10 +0300

    Доработана программа. Сделан подсчет массива как с положительными, так и с отрицательными числами.

--- lw3/task1.v1/Vector/VectorProcessor.cpp
+++ lw3/task1.v1/Vector/VectorProcessor.cpp
@@ -1,26 +1,25 @@
-#include "VectorProcessor.h"
+#include "VectorProcessor.h"                                                                                 
 
 #include <cstdlib>
 #include <algorithm> //std::transform
-#include <functional> //std::plus
+
 
 void ProcessVector(std::vector<double> & numbers)
 {
-	if (numbers.size() == 1)
-	{
-		if (numbers[0] > 0)
-		{
-			numbers[0] += numbers[0];
-		}
-	}
-	else
-	{
-		double avg = 0;
-		for (size_t i = 0; i < numbers.size(); ++i)
-		{
-			avg += numbers[i];
-		}
-		avg /= numbers.size();
-		std::transform(numbers.begin(), numbers.end(), numbers.begin(), bind2nd(std::plus<double>(), avg));
-	}
+    size_t positivesCount = 0;
+    double positivesSum = 0;
+    for (auto number : numbers)
+    {
+        if (number > 0)
+        {
+            ++positivesCount;
+            positivesSum += number;
+        }
+    }
+    const double positivesAvg = (positivesCount > 0) ? positivesSum / positivesCount : 0;
+
+    std::transform(numbers.begin(), numbers.end(), numbers.begin(), 
+        [positivesAvg](double item){
+            return item + positivesAvg;
+    });
 }
--- lw3/task1.v1/VectorTest/VectorTest.cpp
+++ lw3/task1.v1/VectorTest/VectorTest.cpp
@@ -5,50 +5,23 @@
 #include <vector>
 #include "Vector/VectorProcessor.h"
 
-BOOST_AUTO_TEST_CASE(TestThatComputerWorksCorrectly)
+bool VerifyVectorProcessing(std::vector<double> inputNumbers, std::vector<double> const& expectedNumbers)
 {
-    int x = 2;
-    int y = 2;
-    BOOST_CHECK_EQUAL(x + y, 4);
-
-    std::string hello("Hello, Unit Test!");
-    BOOST_CHECK(!hello.empty());
-
-    BOOST_CHECK_NE(4, 5);
+    ProcessVector(inputNumbers);
+    return (inputNumbers == expectedNumbers);
 }
 
 BOOST_AUTO_TEST_SUITE(VectorProcessingTests)
 
-BOOST_AUTO_TEST_CASE(EmptyVector)
+BOOST_AUTO_TEST_CASE(AllTestCases)
 {
-    std::vector<double> empty;
-    ProcessVector(empty);
-	BOOST_CHECK(empty.empty());
-}
-
-BOOST_AUTO_TEST_CASE(VectorContainingSinglePositiveItem)
-{
-	std::vector<double> singleItemVector = { 42 };
-	ProcessVector(singleItemVector);
-	BOOST_CHECK_EQUAL(singleItemVector.size(), 1u);
-	BOOST_CHECK_EQUAL(singleItemVector.front(), 84.0);
-}
-
-BOOST_AUTO_TEST_CASE(VectorContainingSingleNegativeItem)
-{
-	std::vector<double> singleItemVector = { -42 };
-	ProcessVector(singleItemVector);
-	BOOST_CHECK_EQUAL(singleItemVector.size(), 1u);
-	BOOST_CHECK_EQUAL(singleItemVector.front(), -42.0);
-}
-
-BOOST_AUTO_TEST_CASE(VectorContainingTwoPositiveItems)
-{
-	std::vector<double> twoItems = {2, 4};
-	ProcessVector(twoItems);
-	BOOST_CHECK_EQUAL(twoItems.size(), 2u);
-	std::vector<double> const expectedItems = {5, 7};
-	BOOST_CHECK(twoItems == expectedItems);
+    BOOST_CHECK(VerifyVectorProcessing({}, {}));
+    BOOST_CHECK(VerifyVectorProcessing({42}, {84.0}));
+    BOOST_CHECK(VerifyVectorProcessing({-42}, {-42.0}));
+    BOOST_CHECK(VerifyVectorProcessing({2,4}, {5,7}));
+    BOOST_CHECK(VerifyVectorProcessing({0}, {0}));
+    BOOST_CHECK(VerifyVectorProcessing({42, -42},{84, 0}));
+    BOOST_CHECK(VerifyVectorProcessing({1, 2},{2.5, 3.5}));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
