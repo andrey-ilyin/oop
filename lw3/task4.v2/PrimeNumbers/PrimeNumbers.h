212544e9f6d62c5f0ae191157ef86268ce41cd6a
Andruxa Ilyin <andruxa.ilyin@gmail.com>
2014-12-19 19:25:06 +0300

    Initial commit

---
+++ lw3/task4.v2/PrimeNumbers/PrimeNumbers.h
@@ -0,0 +1,7 @@
+#ifndef PRIME_NUMBERS_H
+#define PRIME_NUMBERS_H
+
+
+
+
+#endif //PRIME_NUMBERS_H
---
+++ lw3/task4.v2/PrimeNumbersTest/PrimeNumbersTest.cpp
@@ -0,0 +1,13 @@
+#define BOOST_AUTO_TEST_MAIN
+
+#include <boost/test/included/unit_test.hpp>
+#include <string>
+#include "PrimeNumbers/PrimeNumbers.h"
+
+BOOST_AUTO_TEST_SUITE(PrimeNumbersTests)
+
+BOOST_AUTO_TEST_CASE(AllTestCases)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
