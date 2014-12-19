fe293006a5ee1a8e858db26184f4136621a089b1
Andruxa Ilyin <andruxa.ilyin@gmail.com>
2014-12-19 19:15:08 +0300

    Дораобтана программа для удаления лишних пробелов.

--- lw3/task2.v1/RemoveExtraSpaces/main.cpp
+++ lw3/task2.v1/RemoveExtraSpaces/main.cpp
@@ -1,6 +1,14 @@
+#include <iostream>
+#include <string>
 #include "RemoveExtraSpaces.h"
 
 int main(int argc, char* argv[])
 {
+	std::string text;
+	std::getline(std::cin, text);
+
+	std::cout << "#" << text << "#" << std::endl;
+	std::cout << "#" << RemoveExtraSpaces(text) << "#" << std::endl;
+
     return 0;
 }
