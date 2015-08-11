3becdf4bf514fd3db38b3fe88df807ff3fe0f4ed
Andrey Ilyin <andrey.ilyin@ispringsolutions.com>
2015-08-11 12:03:29 +0300

    Исправлены замечания: 1. Убрал присваивание "" строке. Конструктор по умолчанию и без этого все правильно инициирует.2 2. Использовал функцию reserve для задания ожидаемого размера строки.

--- lw3/task2.v1/RemoveExtraSpaces/RemoveExtraSpaces.cpp
+++ lw3/task2.v1/RemoveExtraSpaces/RemoveExtraSpaces.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 std::string RemoveExtraSpaces(std::string const& arg)
 {
-    std::string result = "";
+    std::string result;
 
     size_t lastNotSpacePosition = arg.find_last_not_of(' ');
     if (lastNotSpacePosition != string::npos)
     {
         size_t firstNotSpacePosition = arg.find_first_not_of(' ');
         firstNotSpacePosition = (firstNotSpacePosition == string::npos) ? 0 : firstNotSpacePosition;
+
+        result.reserve(lastNotSpacePosition - firstNotSpacePosition + 1);
 
         bool space = false;
         for (size_t position = firstNotSpacePosition; position <= lastNotSpacePosition; ++position)
